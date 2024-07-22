a0c7ba14fb4973933cd36fec1ccf3973402021a9
scheerlaaws <srinivascheerla009@gmail.com>
2024-07-23 01:33:28 +0530

    Update variable.tf
    fix

--- variable.tf
+++ variable.tf
@@ -39,7 +39,17 @@ variable "ami_description" {
   description = "(Required) Choose a description for the AMI"
   default     = ""
 }
+variable "aws_access_key" {
+  description = "AWS access key"
+}
 
+variable "aws_secret_key" {
+  description = "AWS secret key"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+}
 variable "instance_types" {
   type        = list(string)
   description = <<-EOD
